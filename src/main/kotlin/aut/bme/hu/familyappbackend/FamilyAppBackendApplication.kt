6333b268861a998c2386d73c29ccfdd1cef5d8c3
Vida Péter <vida.peter1127@gmail.com>
2022-02-20 16:02:18 +0100

    Initial commit

---
+++ src/main/kotlin/aut/bme/hu/familyappbackend/FamilyAppBackendApplication.kt
@@ -0,0 +1,11 @@
+package aut.bme.hu.familyappbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FamilyAppBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<FamilyAppBackendApplication>(*args)
+}
