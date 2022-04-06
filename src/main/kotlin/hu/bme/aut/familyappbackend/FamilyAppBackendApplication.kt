ee590bb68eee4ea3f98eadd247f8893b03622e9a
Vida Péter <vida.peter1127@gmail.com>
2022-04-06 21:40:44 +0200

    mapper problem

--- src/main/kotlin/hu/bme/aut/familyappbackend/FamilyAppBackendApplication.kt
+++ src/main/kotlin/hu/bme/aut/familyappbackend/FamilyAppBackendApplication.kt
@@ -1,9 +1,10 @@
 package hu.bme.aut.familyappbackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class FamilyAppBackendApplication
 
 fun main(args: Array<String>) {
